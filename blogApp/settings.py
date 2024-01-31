258ef08efa0c3b3114df7cffc0c9cb57c34bcb7a
Robert Alvarez <alvarezrobert150@gmail.com>
2024-01-31 12:02:14 -0400

    add static files

--- blogApp/settings.py
+++ blogApp/settings.py
@@ -141,10 +141,10 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
-if DEBUG:
-  STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-else:
-  STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+if not DEBUG:
   STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
